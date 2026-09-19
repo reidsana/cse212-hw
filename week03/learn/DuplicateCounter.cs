ad937273955df7642d751b0b4fc5bf73b2bec760
reidsana <oksanareid48@gmail.com>
2026-09-18 21:38:24 -0500

    learn

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -24,7 +24,24 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        {
+            var seen = new HashSet<int>();
+            int duplicates = 0;
+
+            foreach (int number in data)
+            {
+                if (seen.Contains(number))
+                {
+                    duplicates++;
+                }
+                else
+                {
+                    seen.Add(number);
+                }
+            }
+
+            return duplicates;
+        }
+
     }
 }
